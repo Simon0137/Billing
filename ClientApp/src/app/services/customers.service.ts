da26db4e74b1cdd564ae1043443ffa338f017b05
Simon0137 <simon.leybov@gmail.com>
2023-09-30 11:42:30 +0300

    Fix customers.service.ts

--- ClientApp/src/app/services/customers.service.ts
+++ ClientApp/src/app/services/customers.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Customer } from '../types/customer';
 import { lastValueFrom } from 'rxjs';
 
@@ -9,14 +9,14 @@ export class CustomersService {
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 
   async addCustomerAsync(customer: Customer): Promise<void> {
-    await lastValueFrom(this.http.post(`${this.baseUrl}/api/customers`, customer));
+    await lastValueFrom(this.http.post(`${this.baseUrl}api/customers`, customer));
   }
 
   async loadCustomersAsync(): Promise<Customer[]> {
-    return await lastValueFrom(this.http.get<Customer[]>(`${this.baseUrl}/api/customers`));
+    return await lastValueFrom(this.http.get<Customer[]>(`${this.baseUrl}api/customers`));
   }
 
   async deleteCustomerAsync(id: number): Promise<void> {
-    await lastValueFrom(this.http.delete(`${this.baseUrl}/api/customers/${id}`));
+    await lastValueFrom(this.http.delete(`${this.baseUrl}api/customers/${id}`));
   }
 }
